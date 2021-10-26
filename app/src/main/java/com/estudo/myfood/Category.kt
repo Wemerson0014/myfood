d68b1517065960cbefdf0a01863745bee3be75f2
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2021-10-26 15:57:46 -0300

    Lista de categorias com RecyclerView e Generics

---
+++ app/src/main/java/com/estudo/myfood/Category.kt
@@ -0,0 +1,8 @@
+package com.estudo.myfood
+
+data class Category(
+    val id: Int,
+    val logoUrl: String,
+    val name: String,
+    val color: Long
+)
--- app/src/main/java/com/estudo/myfood/RestaurantFragment.kt
+++ app/src/main/java/com/estudo/myfood/RestaurantFragment.kt
@@ -3,11 +3,15 @@ package com.estudo.myfood
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import co.tiagoaguiar.atway.ui.adapter.ATAdapter
 import com.estudo.myfood.databinding.FragmentRestaurantBinding
 
 class RestaurantFragment : Fragment(R.layout.fragment_restaurant) {
 
     private var binding: FragmentRestaurantBinding? = null
+
+    private val categoryAdapter = ATAdapter({ CategoryView(it) })
 
     private var filters = arrayOf(
         FilterItem(0, "Ordernar", closeIcon = R.drawable.ic_baseline_keyboard_arrow_down_24),
@@ -23,8 +27,52 @@ class RestaurantFragment : Fragment(R.layout.fragment_restaurant) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        categoryAdapter.items = arrayListOf(
+            Category(
+                1,
+                "https://www.ifood.com.br/static/images/categories/market.png",
+                "Mercado",
+                0xFFB6D048
+            ),
+            Category(
+                2,
+                "https://www.ifood.com.br/static/images/categories/restaurant.png",
+                "Restaurante",
+                0xFFE91D2D
+            ),
+            Category(
+                3,
+                "https://www.ifood.com.br/static/images/categories/drinks.png",
+                "Bebidas",
+                0xFFF6D553
+            ),
+            Category(
+                4,
+                "https://static-images.ifood.com.br/image/upload/f_auto/webapp/landingV2/express.png",
+                "Express",
+                0xFFFF0000
+            ),
+            Category(
+                5,
+                "https://parceiros.ifood.com.br/static/media/salad.9db040c0.png",
+                "Saudável",
+                0xFFE91D2D
+            ),
+            Category(
+                6,
+                "https://static-images.ifood.com.br/image/upload/t_high/discoveries/Salgados-out_20_o4cW.jpg",
+                "Salgados",
+                0xFF8C60C5
+            )
+        )
+
         binding = FragmentRestaurantBinding.bind(view)
         binding?.let {
+
+            it.rvCategory.layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            it.rvCategory.adapter = categoryAdapter
+
             filters.forEach { filter ->
                 it.chipGrouoFilter.addView(filter.toChip(requireContext()))
             }
